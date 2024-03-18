4bd44a0cb1ecb269cefd2e711f190488f4ea19ea
ahmed <a.badawi@tuwaiq.edu.sa>
2024-03-18 14:57:17 +0300

    fix

--- ServerCheck/publish-prod.sh
+++ ServerCheck/publish-prod.sh
@@ -1,10 +1,11 @@
+VERSION="v2.0"
 cd ..
 
-docker image rm docker.tuwaiqdev.com/tuwaiq-hadaf-service:latest
-docker image rm docker.tuwaiqdev.com/tuwaiq-hadaf-service:v1.0
-docker image rm tuwaiq-hadaf-service:latest
-docker image rm tuwaiq-hadaf-service:v1.0
-docker build -t docker.tuwaiqdev.com/tuwaiq-hadaf-service:v1.0 -f Tuwaiq-ServerCheck/Dockerfile . #--no-cache
-docker tag docker.tuwaiqdev.com/tuwaiq-hadaf-service:v1.0 docker.tuwaiqdev.com/tuwaiq-hadaf-service:latest
-docker push docker.tuwaiqdev.com/tuwaiq-hadaf-service:v1.0
-docker push docker.tuwaiqdev.com/tuwaiq-hadaf-service:latest
+docker image rm docker.tuwaiqdev.com/tuwaiq-gosi-service:latest
+docker image rm docker.tuwaiqdev.com/tuwaiq-gosi-service:$VERSION
+docker image rm tuwaiq-gosi-service:latest
+docker image rm tuwaiq-gosi-service:$VERSION
+docker buildx build --platform linux/amd64 -t docker.tuwaiqdev.com/tuwaiq-gosi-service:$VERSION -f ServerCheck/Dockerfile . #--no-cache
+docker tag docker.tuwaiqdev.com/tuwaiq-gosi-service:$VERSION docker.tuwaiqdev.com/tuwaiq-gosi-service:latest
+docker push docker.tuwaiqdev.com/tuwaiq-gosi-service:$VERSION
+docker push docker.tuwaiqdev.com/tuwaiq-gosi-service:latest
